11d102ecb9771d70e4dc487440438e56fd8d2e14
experiencei <ayelojahighbee01@gmail.com>
2021-11-21 12:44:34 +0100

    adding socket io into the server

--- index.js
+++ index.js
@@ -3,18 +3,35 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
+const users = []
 const port = 3001
 
 app.get("/"  , (req , res) => {
     res.send("Hello Worlda")
 })
 
+const addUser = (userName , roomId) => {
+    users.push({
+        userName : userName,
+        roomId : roomId
+    })
+}
+const getAllUsers = (roomId) => {
+   return users.filter(user => user.roomId === roomId)
+}
+
 io.on("connection" , socket => {
     console.log("someone connected");
-    socket.on("join-room" , ({ roomId , userId}) => {
+    socket.on("join-room" , ({ roomId , userName}) => {
         console.log("joine");
         console.log(roomId);
-        console.log(userId);
+        console.log(userName);
+        socket.join(roomId);
+        addUser(userName , roomId);  
+        socket.to(roomId).emit("user-connected" , userName)
+
+        io.emit
+        
     })
 })
 
